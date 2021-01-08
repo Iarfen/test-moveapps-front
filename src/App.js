09e4491ae197aa7284b6501186b1b61dc9fefe23
Ismael <ismael.correa.castro@gmail.com>
2021-01-08 03:57:46 -0300

    Front-end app complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment, useState, Component } from "react";
+import { Layout, Menu, Table } from 'antd';
+import 'antd/dist/antd.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const axios = require('axios');
+const userTableColumns = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id'
+    },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email'
+  },
+  {
+    title: 'Password',
+    dataIndex: 'password',
+    key: 'password'
+  },
+  {
+    title: 'Phones',
+    dataIndex: 'phones',
+    key: 'phones',
+    render: user_phones => (
+      <>
+        {user_phones.map(user_phone => {
+          return (
+            <div>
+              +{user_phone.countryCode} {user_phone.cityCode} {user_phone.number}
+            </div>
+          );
+        })}
+      </>
+    ),
+  }
+];
+
+class App extends Component {
+
+  render() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider>
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+      <Menu.Item key="1"><Link to="/">Inicio</Link></Menu.Item>
+      <Menu.Item key="2"><Link to="/users">Usuarios</Link></Menu.Item>
+    </Menu>
+    </Sider>
+    <Layout className="site-layout">
+        <div id="layout_content">
+        <Switch>
+            <Route path="/users">
+                <Users />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+        </div>
+    </Layout>
+    </Layout>
+    </Router>
+  );
+    }
+}
+
+function Home() {
+  return <h1 id="home_h1">Bienvenido!</h1>;
+}
+
+function Users() {
+    const [usuarios, setUsuarios] = useState([]);
+
+    axios.get('http://localhost:8081/users')
+  .then(function (response) {
+    setUsuarios(response.data);
+    console.log(usuarios);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  return (
+      <Fragment>
+      <h1>Usuarios</h1>
+      <Table columns={userTableColumns} dataSource={usuarios}>
+      </Table>
+      </Fragment>
   );
 }
 
